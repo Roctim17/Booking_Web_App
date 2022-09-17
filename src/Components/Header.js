aeb2a441585862fc576fc8defc90acf8ea21dc5b
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-09-18 02:29:35 +0600

    header part

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,31 +8,39 @@ import {
     faPlane,
     faTaxi, faBed
 } from "@fortawesome/free-solid-svg-icons"
+import '../App.css';
 
 const Header = () => {
     return (
         <div className='header'>
-            <div className="headerList">
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faHotel} />
-                    <span>Stays</span>
+            <div className="headerContainer">
+                <div className="headerList">
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faHotel} />
+                        <span>Stays</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faPlane} />
+                        <span>Flights</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faCar} />
+                        <span>Car rentals</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faBed} />
+                        <span>Attractions</span>
+                    </div>
+                    <div className="headerListItem">
+                        <FontAwesomeIcon icon={faTaxi} />
+                        <span>Airport taxis</span>
+                    </div>
                 </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faPlane} />
-                    <span>Flights</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faCar} />
-                    <span>Car rentals</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faBed} />
-                    <span>Attractions</span>
-                </div>
-                <div className="headerListItem">
-                    <FontAwesomeIcon icon={faTaxi} />
-                    <span>Airport taxis</span>
-                </div>
+                <h1 className='headerTitle'> A Lifetime of discounts</h1>
+                <p className='headerDesc'>
+                    Get rewarded for your travels - unlock instant savings of 105 or more with a free Booking account
+                </p>
+                <button className='headerBtn'>Sign in / Register</button>
             </div>
         </div>
     );

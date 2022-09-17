a818357aa6d42d9f3ba9fd05a6f2279dae443bf8
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-09-18 03:03:24 +0600

    add calander

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import {
     faHotel,
-    // faCalendarDays,
     faCar,
-    // faPerson,
     faPlane,
-    faTaxi, faBed
+    faTaxi, faBed, faCalendarDays, faPerson
 } from "@fortawesome/free-solid-svg-icons"
 import '../App.css';
+import { DateRange } from 'react-date-range';
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 const Header = () => {
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    ]);
     return (
         <div className='header'>
             <div className="headerContainer">
                 <div className="headerList">
-                    <div className="headerListItem">
+                    <div className="headerListItem active">
                         <FontAwesomeIcon icon={faHotel} />
                         <span>Stays</span>
                     </div>
@@ -41,6 +49,31 @@ const Header = () => {
                     Get rewarded for your travels - unlock instant savings of 105 or more with a free Booking account
                 </p>
                 <button className='headerBtn'>Sign in / Register</button>
+                <div className="headerSearch">
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faHotel} className="headerIcon" />
+                        <input
+                            type="text"
+                            placeholder="Where are you going?"
+                            className="headerSearchInput"
+                        />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faCalendarDays} className="headerIcon" />
+                        <span className='headerSearchText'>date to date</span>
+                        <DateRange
+                            className='date'
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                        />
+                    </div>
+                    <div className="headerSearchItem">
+                        <FontAwesomeIcon icon={faPerson} className="headerIcon" />
+                        <span className='headerSearchText'>2 adults 2 children 1 room</span>
+                    </div>
+                </div>
             </div>
         </div>
     );

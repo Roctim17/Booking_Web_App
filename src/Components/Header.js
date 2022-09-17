0ef6c07669ed0cab2b5b2bc22a7d5c133f984e1a
Roctim17 <mdrokibulislamroctim@gmail.com>
2022-09-18 01:28:56 +0600

    break

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import {
+    faHotel,
+    // faCalendarDays,
+    faCar,
+    // faPerson,
+    faPlane,
+    faTaxi, faBed
+} from "@fortawesome/free-solid-svg-icons"
 
 const Header = () => {
     return (
-        <div>
-
+        <div className='header'>
+            <div className="headerList">
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faHotel} />
+                    <span>Stays</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faPlane} />
+                    <span>Flights</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faCar} />
+                    <span>Car rentals</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faBed} />
+                    <span>Attractions</span>
+                </div>
+                <div className="headerListItem">
+                    <FontAwesomeIcon icon={faTaxi} />
+                    <span>Airport taxis</span>
+                </div>
+            </div>
         </div>
     );
 };
